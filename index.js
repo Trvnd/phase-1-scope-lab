d36ad969354302ecf38d997f3918a54fcba817e0
Jimmy Senecal <jamesnsenecal@gmail.com>
2022-04-25 23:47:40 -0400

    commit tree e319b0a8a3856e945cf8824470c88e4cae9769b1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+var customerName = 'bob'
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+    return upperCaseCustomerName;
+}
+upperCaseCustomerName();
+
+
+
